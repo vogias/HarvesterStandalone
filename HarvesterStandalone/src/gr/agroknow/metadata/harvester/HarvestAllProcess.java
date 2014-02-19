e9b591421259325d9be528b9cf2b72884daa128e
vogias <vogias@admin.grnet.gr>
2014-02-19 12:13:14 +0200

    c

--- HarvesterStandalone/src/gr/agroknow/metadata/harvester/HarvestAllProcess.java
+++ HarvesterStandalone/src/gr/agroknow/metadata/harvester/HarvestAllProcess.java
@@ -3,7 +3,6 @@ package gr.agroknow.metadata.harvester;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Properties;
