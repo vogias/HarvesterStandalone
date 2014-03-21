b6a3ddb56001df17fcc8853d08e9304c6e4f9585
vogias <vogias@admin.grnet.gr>
2014-03-21 12:58:51 +0200

    full harvesting option with no timestamps is been added

--- HarvesterStandalone/src/gr/agroknow/metadata/harvester/HarvestAllProcess.java
+++ HarvesterStandalone/src/gr/agroknow/metadata/harvester/HarvestAllProcess.java
@@ -41,92 +41,110 @@ public class HarvestAllProcess {
 	public static void listRecords(String target, String folderName,
 			String metadataPrefix, String set) throws OAIException,
 			IOException, JDOMException {
+		OAIRepository repos = null;
+		try {
+			repos = new OAIRepository();
 
-		OAIRepository repos = new OAIRepository();
-		File file = new File(folderName);
-		String identifier = "";
-		file.mkdirs();
+			File file = new File(folderName);
+			String identifier = "";
+			file.mkdirs();
 
-		repos.setBaseURL(target);
-		
-		System.out.println("Max retry limit:" + repos.getRetryLimit());
+			repos.setBaseURL(target);
 
-		OAIRecordList records;
+			System.out.println("Max retry limit:" + repos.getRetryLimit());
 
-		// OAIRecordList records =
-		// repos.listRecords("ese","9999-12-31","2000-12-31","");
-		Properties props = new Properties();
-		props.load(new FileInputStream("configure.properties"));
+			OAIRecordList records;
 
-		String incremental = props.getProperty(Constants.incremental);
-		// String from = "2000-12-31";
+			// OAIRecordList records =
+			// repos.listRecords("ese","9999-12-31","2000-12-31","");
+			Properties props = new Properties();
+			props.load(new FileInputStream("configure.properties"));
 
-		String from = "";
-		String to = "9999-12-31";
+			String incremental = props.getProperty(Constants.incremental);
+			// String from = "2000-12-31";
 
-		if (incremental.equalsIgnoreCase("true")) {
-			from = props.getProperty(Constants.lhDate);
-			System.out.println("Last harvesting date:" + from);
-		} else if (incremental.equalsIgnoreCase("false")) {
-			from = "1900-12-31";
-			System.out.println("Fallback last harvesting date:" + from);
-		} else {
-			System.err.println("Wrong harvester.incremental value");
-			System.exit(-1);
-		}
+			String from = "";
+			String to = "9999-12-31";
 
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		String date = dateFormat.format(new Date());
+			if (incremental.equalsIgnoreCase("true")) {
+				from = props.getProperty(Constants.lhDate);
+				System.out.println("Last harvesting date:" + from);
+			} else if (incremental.equalsIgnoreCase("false")) {
+				from = "1900-12-31";
+				System.out.println("Fallback last harvesting date:" + from);
+			} else {
+				System.err.println("Wrong harvester.incremental value");
+				System.exit(-1);
+			}
 
-		System.out.println("Harvesting date:" + date);
-		System.out
-				.println("Harvesting repository:" + repos.getRepositoryName());
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			String date = dateFormat.format(new Date());
 
-		if (set.equals(""))
-			records = repos.listRecords(metadataPrefix, to, from);
-		else
-			records = repos.listRecords(metadataPrefix, to, from, set);
+			System.out.println("Harvesting date:" + date);
+			System.out.println("Harvesting repository:"
+					+ repos.getRepositoryName());
 
-		// records = repos.listRecords(metadataPrefix);
+			if (set.equals("")) {
 
-		int counter = 0;
-		int deletedRecords = 0;
-		// records.moveNext();
-		while (records.moreItems()) {
-			StringBuffer logString = new StringBuffer();
+				if (from.equals("1900-12-31"))
+					records = repos.listRecords(metadataPrefix);
+				else
+					records = repos.listRecords(metadataPrefix, to, from);
+			}
 
-			logString.append(file.getName());
-			logString.append(" " + metadataPrefix);
-			logString.append(" " + "ALL");
+			else {
+				System.out.println("Set:" + set);
+				records = repos.listRecords(metadataPrefix, to, from, set);
+			}
 
-			OAIRecord item = records.getCurrentItem();
+			// records = repos.listRecords(metadataPrefix);
 
-			/*
-			 * get the lom metadata : item.getMetadata(); this return a Node
-			 * which contains the lom metadata.
-			 */
-			if (!item.deleted()) {
-				Element metadata = item.getMetadata();
+			int counter = 0;
+			int deletedRecords = 0;
+			// records.moveNext();
+			while (records.moreItems()) {
 
-				logString.append(" " + "NEW");
+				StringBuffer logString = new StringBuffer();
 
-				if (metadata != null) {
-					// System.out.println(item.getIdentifier());
-					counter++;
-					Record rec = new Record();
-					rec.setOaiRecord(item);
-					rec.setMetadata(item.getMetadata());
-					rec.setOaiIdentifier(item.getIdentifier());
+				logString.append(file.getName());
+				logString.append(" " + metadataPrefix);
+				logString.append(" " + "ALL");
 
-					logString.append(" " + item.getIdentifier());
+				OAIRecord item = records.getCurrentItem();
 
-					identifier = item.getIdentifier().replaceAll(":", "_");
-					identifier = identifier.replaceAll("/", ".");
-					identifier = identifier.replaceAll("\\?", ".");
-					IOUtilsv2.writeStringToFileInEncodingUTF8(
-							OaiUtils.parseLom2Xmlstring(metadata), folderName
-									+ "/" + identifier + ".xml");
+				/*
+				 * get the lom metadata : item.getMetadata(); this return a Node
+				 * which contains the lom metadata.
+				 */
+				if (!item.deleted()) {
+					Element metadata = item.getMetadata();
 
+					logString.append(" " + "NEW");
+
+					if (metadata != null) {
+						// System.out.println(item.getIdentifier());
+						counter++;
+						Record rec = new Record();
+						rec.setOaiRecord(item);
+						rec.setMetadata(item.getMetadata());
+						rec.setOaiIdentifier(item.getIdentifier());
+
+						logString.append(" " + item.getIdentifier());
+
+						identifier = item.getIdentifier().replaceAll(":", "_");
+						identifier = identifier.replaceAll("/", ".");
+						identifier = identifier.replaceAll("\\?", ".");
+						IOUtilsv2.writeStringToFileInEncodingUTF8(
+								OaiUtils.parseLom2Xmlstring(metadata),
+								folderName + "/" + identifier + ".xml");
+
+					} else {
+
+						logString.append(" " + "DELETED");
+						logString.append(" " + item.getIdentifier());
+						deletedRecords++;
+
+					}
 				} else {
 
 					logString.append(" " + "DELETED");
@@ -134,19 +152,18 @@ public class HarvestAllProcess {
 					deletedRecords++;
 
 				}
-			} else {
-
-				logString.append(" " + "DELETED");
-				logString.append(" " + item.getIdentifier());
-				deletedRecords++;
-
+				slf4jLogger.info(logString.toString());
+				records.moveNext();
 			}
-			slf4jLogger.info(logString.toString());
-			records.moveNext();
+			// System.out.println(counter);
+			System.out.println("Records harvested:" + counter);
+		} catch (OAIException ex) {
+			System.err
+					.println("Harvesting from "
+							+ repos.getRepositoryName()
+							+ " did not complete because of a harvesting error, the error was : "
+							+ ex.getMessage());
 		}
-		// System.out.println(counter);
-		System.out.println("Records harvested:" + counter);
 
 	}
-
 }
